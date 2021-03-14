4edfee3ecc9dcb8ffe9bf3a67b8780a8a594bf2d
Rishikesh Rachchh <rishikeshisbest29@gmail.com>
2021-03-14 23:04:25 +0400

    Bump version: 2.0.5 → 2.0.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='autojoiner',
     packages=['autojoiner'],
     # description='',
-    version='2.0.5',
+    version='2.0.6',
     install_requires=[
         'click', 'pyautogui', 'selenium',
     ],
