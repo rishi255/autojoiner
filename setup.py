db68d3fb1fa8f3eab651a57bacca1cd218f86281
Rishikesh Rachchh <rishikeshrachchh@gmail.com>
2021-03-14 20:08:56 +0400

    Removed unnecessary code

--- setup.py
+++ setup.py
@@ -1,32 +1,22 @@
 from setuptools import setup
-# from io import open
-# from os import path
-
-import pathlib
-
-# The directory containing this file
-HERE = pathlib.Path(__file__).parent
-
-# The text of the README file
-README = (HERE / "README.md").read_text()
 
 setup(
     name='autojoiner',
-    version='2.0',
     packages=['autojoiner'],
+    # description='',
+    version='2.0',
     install_requires=[
-        'click', 'pyautogui', 'selenium', 'cryptography'
+        'click', 'pyautogui', 'selenium', 'setuptools-rust', 'cryptography'
     ],
-    python_requires='>=2.7',
+    python_requires='>=3.8',
     entry_points='''
         [console_scripts]
-        autojoiner=src.__main__:main
+        autojoiner=autojoiner.__main__:main
     ''',
     author="Rishikesh Rachchh",
-    long_description=README,
-    long_description_content_type="text/markdown",
+    author_email="rishikeshrachchh@gmail.com",
     license='MIT',
     url="https://github.com/rishi255/autojoin-v2",
-    author_email="rishikeshrachchh@gmail.com",
+    download_url='https://github.com/rishi255/autojoin-v2/archive/v2.tar.gz',
     # dependency_links=dependency_links
 )
