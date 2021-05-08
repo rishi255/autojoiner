7c475f2ca998a9c1de4ad316c39b898015c7661f
Rishikesh Rachchh <rishikeshrachchh@gmail.com>
2021-05-09 00:13:51 +0400

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -1,29 +1,30 @@
 from setuptools import setup
 
 setup(
-    name='autojoiner',
-    packages=['autojoiner'],
+    name="autojoiner",
+    packages=["autojoiner"],
     # description='',
-    version='0.0.4',
+    version="0.1.2",
     install_requires=[
-        'click', 
-        'selenium',
+        "click",
+        "selenium",
     ],
-     extras_require={
-        'gui': ['PyAutoGUI'], # this doesn't exist on TestPyPi, hence creates issues -> Made it optional
+    extras_require={
+        "gui": [
+            "PyAutoGUI"
+        ],  # this doesn't exist on TestPyPi, hence creates issues -> Made it optional
     },
-    python_requires='>=3.8',
-    entry_points='''
+    python_requires=">=3.8",
+    entry_points="""
         [console_scripts]
         autojoiner=autojoiner.__main__:main
-    ''',
+    """,
     author="Rishikesh Rachchh",
     author_email="rishikeshrachchh@gmail.com",
-    classifiers = [
-        'Development Status :: 2 - Pre-Alpha',
-        'License :: OSI Approved :: MIT License',
-
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
     ],
-    license='MIT',
-    url="https://github.com/rishi255/autojoiner"
+    license="MIT",
+    url="https://github.com/rishi255/autojoiner",
 )
