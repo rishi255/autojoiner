bf9190dbb5396ca332c72c08b3502bb94b368069
Rishikesh Rachchh <rishikeshrachchh@gmail.com>
2021-05-09 00:15:34 +0400

    Fixes

--- tasks.py
+++ tasks.py
@@ -1,27 +1,39 @@
 from invoke import task
+import platform
+
 
 @task
 def clean(c):
-	c.run("rm -rf dist build autojoiner.egg-info")
+    if platform.system() == "Windows":
+        c.run("rmdir /s /q dist; rmdir /s /q build; rmdir /s /q autojoiner.egg-info")
+    else:
+        c.run("rm -rf dist build autojoiner.egg-info")
+    return
 
-@task 
+
+@task
 def make(c):
-	clean(c)
-	c.run("python3 setup.py sdist bdist_wheel")
+    # clean(c)
+    if platform.system() == "Windows":
+        c.run("py setup.py sdist bdist_wheel")
+    else:
+        c.run("python3 setup.py sdist bdist_wheel")
+
 
 @task
 def upload(c, actual=False):
-	if actual:
-		print("ACTUAL CALLED!")
-		# c.run("twine upload dist/*")
-	else:
-		print("Test upload.")
-		c.run("twine upload --repository-url https://test.pypi.org/legacy/ dist/*")
+    if actual:
+        print("ACTUAL CALLED!")
+        # c.run("twine upload dist/*")
+    else:
+        print("Test upload.")
+        c.run("twine upload --repository-url https://test.pypi.org/legacy/ dist/*")
+
 
 @task
 def bump(c, part="patch", actual=False):
-	if actual:
-		c.run(f"bumpversion {part} --allow-dirty --verbose --commit --tag")
-		print("**** NOT A DRY RUN! Version actually bumped. **** ")
-	else:
-		c.run(f"bumpversion {part} --allow-dirty --verbose --commit --tag --dry-run")
+    if actual:
+        c.run(f"bumpversion {part} --allow-dirty --verbose --commit --tag")
+        print("**** NOT A DRY RUN! Version actually bumped. **** ")
+    else:
+        c.run(f"bumpversion {part} --allow-dirty --verbose --commit --tag --dry-run")
